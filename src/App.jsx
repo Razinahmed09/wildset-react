ee2a5eafd74a27d06000ac9a832bb325ea685b4a
razin.ahmed <raheenahmee@gmail.com>
2024-07-29 02:28:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function navigation() {
+
+  return (
+    <>
+      <div className='container-fluid'>
+        <ul className='flex gap-3 justify-center bg-black text-white' >
+          <li>Care Guide</li>
+          <li>Best Pet For You</li>
+          <li>Fing Suppliers</li>
+          <li>Build a Habitat</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default navigation
